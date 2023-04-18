38ecf6c815a8c1790f63a2dc118b9be2bffbab3d
Aeoxht <114609616+Aeoxht@users.noreply.github.com>
2023-04-18 19:53:35 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
 save()
 {
+  const a = document.createElement('a');
+  const b = new Blob([JSON.strigify("")]);
+  a.href = URL.createObjectURL(b);
+  a.download = 'test';
+  a.click();
   console.log();
 }
